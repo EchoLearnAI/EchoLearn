a156689f42c3f1317f51e53273c72a5f8ae8aa0f
thiago.justo <thiago.justo@fueled.com>
2024-12-23 13:50:42 +0100

    chore: remove old way

--- echolearn-iac/init/locals.tf
+++
@@ -1,5 +0,0 @@
-data "azurerm_subscription" "current" {}
-
-locals {
-  tags = merge(var.tags, data.azurerm_subscription.current.tags)
-}
--- echolearn-iac/init/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "location" { type = string }
-variable "project_name" { type = string }
-variable "environment" { type = string }
-variable "subscription_id" { type = string }
-
-variable "tags" {
-  type    = map(string)
-  default = {}
-}
