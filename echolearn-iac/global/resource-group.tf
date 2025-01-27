951fcee22f10fd38d59f4ac17e2f607a6c98924d
thiago-cruz_mars <thiago.cruz@effem.com>
2025-01-27 19:50:22 +0100

    chore: add global module

---
+++ echolearn-iac/global/resource-group.tf
@@ -0,0 +1,13 @@
+module "naming_global" {
+  source      = "../modules/naming"
+  name        = var.name
+  environment = var.environment
+  location    = var.location
+  increment   = var.increment
+}
+
+resource "azurerm_resource_group" "global" {
+  name = module.naming_global.resource_group.name
+  location = var.location
+  tags = local.tags
+}
