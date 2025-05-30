147bd8de3532bf72fa16412b224299bc3590fb09
Thiago Cruz <thagocruz@gmail.com>
2025-05-30 18:18:22 +0200

    chore: add new copy

--- EchoLearn-ReactNative/index.js
+++
@@ -1,5 +0,0 @@
-import {AppRegistry} from 'react-native';
-import App from './src/App'; // Adjusted path
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App); 
